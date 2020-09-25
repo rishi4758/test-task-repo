3b4eb1e1303da6537c1e8172c8f23ff0b9fb9aab
rishav rapta <rishavrapta79@gmail.com>
2020-09-25 07:00:04 +0530

    test code

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Main from "./components/main";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
