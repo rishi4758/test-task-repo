2dc8ed7de80e37e48e2b7c725572d436f849e062
rishi4758 <47744672+rishi4758@users.noreply.github.com>
2020-10-09 18:11:22 +0530

    Update modal.js

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -9,38 +9,7 @@ import IconButton from "@material-ui/core/IconButton";
 import HighlightOffRoundedIcon from "@material-ui/icons/HighlightOffRounded";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  name: {
-    fontSize: 30,
-    fontWeight: 700,
-  },
-  submitButton: {
-    border: "1px solid #black",
-    backgroundColor: "#0049b7",
-    height: "40px",
-    width: "150px",
 
-    textTransform: "none",
-    borderRadius: "3em",
-    marginTop: "60px",
-    color: "white",
-    fontWeight: "bold",
-    marginRight: "40px",
-    "&:hover": {
-      color: "#6600ff",
-
-      border: "1px solid #ffcc00",
-    },
-  },
-  description: {
-    fontSize: 22,
-    color: "black",
-    fontFamily: "italic",
-    fontWeight: 600,
-    overFlow: "hidden",
-    marginTop: 20,
-  },
-}));
 const modal = (props) => {
   const classes = useStyles();
   const [open, setOpen] = useState(true);
@@ -102,92 +71,36 @@ const modal = (props) => {
 
 export default modal;
 
-//   const renderModal = () => {
-//
-//     if (props.storage.type === "add") {
-//       return (
-//         <Dialog
-//           fullWidth={true}
-//           maxWidth={"sm"}
-//           onClose={handleClose}
-//           aria-labelledby="simple-dialog-title"
-//           open={open}
-//           style={{ textAlign: "center" }}
-//         >
-//           <Grid container justify="center">
-//             <Grid container item xs={12} justify="flex-end">
-//               <IconButton fontSize="large">
-//                 <HighlightOffRoundedIcon />
-//               </IconButton>
-//             </Grid>
-//             <Grid container item xs={12} justify="flex-end">
-//               <DialogContent style={{ textAlign: "center", height: 300 }}>
-//                 {" "}
-//                 <div className={classes.name}>Someone Changed the Data:</div>
-//                 <div
-//                   className={classes.description}
-//                   style={{ width: 350, wordWrap: "break-word" }}
-//                 >
-//                   {" "}
-//                   {props.storage.name} has been increase by{" "}
-//                   {props.storage.count}
-//                 </div>
-//                 <DialogActions>
-//                   <Grid container justify="flex-end">
-//                     <Button
-//                       autoFocus
-//                       onClick={handleClose}
-//                       className={classes.submitButton}
-//                     >
-//                       close
-//                     </Button>
-//                   </Grid>
-//                 </DialogActions>
-//               </DialogContent>
-//             </Grid>
-//           </Grid>
-//         </Dialog>
-//       );
-//     }
-//     if (props.storage.type === "remove") {
-//       return (
-//         <Dialog
-//           maxWidth={"sm"}
-//           onClose={handleClose}
-//           aria-labelledby="simple-dialog-title"
-//           open={open}
-//           style={{ textAlign: "center" }}
-//         >
-//           <Grid container justify="center">
-//             <Grid container item xs={12} justify="flex-end">
-//               <IconButton fontSize="large">
-//                 <HighlightOffRoundedIcon onClick={handleClose} />
-//               </IconButton>
-//             </Grid>
-//             <Grid container item xs={12} justify="flex-end">
-//               <DialogContent style={{ textAlign: "center", height: 200 }}>
-//                 {" "}
-//                 <div className={classes.name}>Someone Changed the Data:</div>
-//                 <div className={classes.description}>
-//                   {" "}
-//                   {props.storage.name} has been increase by{" "}
-//                   {props.storage.count}
-//                 </div>
-//                 <Grid container justify="flex-end">
-//                   <Button
-//                     autoFocus
-//                     onClick={handleClose}
-//                     className={classes.submitButton}
-//                   >
-//                     close
-//                   </Button>
-//                 </Grid>
-//               </DialogContent>
-//             </Grid>
-//           </Grid>
-//         </Dialog>
-//       );
-//     }
-//   };
+const useStyles = makeStyles((theme) => ({
+  name: {
+    fontSize: 30,
+    fontWeight: 700,
+  },
+  submitButton: {
+    border: "1px solid #black",
+    backgroundColor: "#0049b7",
+    height: "40px",
+    width: "150px",
 
-//   return <div>{renderModal()}</div>;
+    textTransform: "none",
+    borderRadius: "3em",
+    marginTop: "60px",
+    color: "white",
+    fontWeight: "bold",
+    marginRight: "40px",
+    "&:hover": {
+      color: "#6600ff",
+
+      border: "1px solid #ffcc00",
+    },
+  },
+  description: {
+    fontSize: 22,
+    color: "black",
+    fontFamily: "italic",
+    fontWeight: 600,
+    overFlow: "hidden",
+    marginTop: 20,
+  },
+}));
+
